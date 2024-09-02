56e0f2546b9d6507dffd1c8a93bde9ce0f47eaa9
Dhananjay Hareesh <dhananjay.hareesh@gmail.com>
2024-09-02 19:05:29 +0530

    testing done

--- test/helpers/test_helper.dart
+++ test/helpers/test_helper.dart
@@ -1,10 +1,14 @@
 import 'package:mockito/annotations.dart';
 import 'package:http/http.dart' as http;
+import 'package:weatherapp_tdd/data/data_sources/remote_data_source.dart';
 import 'package:weatherapp_tdd/domain/repositories/weather_repository.dart';
+import 'package:weatherapp_tdd/domain/usecases/get_current_weather.dart';
 
 @GenerateMocks(
   [
     WeatherRepository,
+    WeatherRemoteDataSource,
+    GetCurrentWeatherUseCase,
   ],
   customMocks: [MockSpec<http.Client>(as: #MockHttpClient)],
 )
